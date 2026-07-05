b4a03d4e1a703bf6aecd9dfda076f379e94707ff
castillolutber-gif <castillolutber@gmail.com>
2026-07-04 21:27:41 -0500

    Delete scripts/fisic.h

--- scripts/fisic.h
+++
@@ -1,9 +0,0 @@
-#include "../entidades/Jugador.h"
-#include "../entidades/Obstaculo.h"
-#include "../entidades/Escenario.h"
-
-class Colisiones {
-public:
-    bool detectarAABB(const Jugador& a, const Obstaculo& b);
-    bool estaEnZona(float x, float y, const Escenario& zona);
-};
