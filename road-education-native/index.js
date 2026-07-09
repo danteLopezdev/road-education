0c9d65164657f69735c2ffa786831907088fb1b2
Dante Lopez Flores <dlf52008lopezf@gmail.com>
2026-07-09 18:32:45 -0500

    Agregando algunos detalles

--- road-education-native/index.js
+++ road-education-native/index.js
@@ -5,5 +5,22 @@
 //
 // 'bindings' localiza el .node compilado sin importar Debug/Release
 // ni el layout exacto de build/.
-
 module.exports = require('bindings')('road_education_native');
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function crearVentana() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false
+    }
+  });
+
+  win.loadFile(path.join(__dirname, '..', 'index.html'));
+}
+
+app.whenReady().then(crearVentana);
--- scripts/ui.js
+++ scripts/ui.js
@@ -1,19 +1,3 @@
-import { cargarPuntajeUsuario } from "./score.js";
-
-export function mostrarPuntaje(puntaje) {
-  const elemento = document.getElementById("puntaje");
-  if (elemento) elemento.textContent = `Puntaje: ${puntaje}`;
-}
-
-export function mostrarUsuario(user) {
-  const elemento = document.getElementById("usuario");
-  if (elemento) elemento.textContent = user ? `Jugador: ${user.displayName}` : "";
-}
-
-export async function inicializarUI(user) {
-  mostrarUsuario(user);
-  if (user) {
-    const puntaje = await cargarPuntajeUsuario(user.uid);
-    mostrarPuntaje(puntaje);
-  }
-}
+document.getElementById('btnJugar').addEventListener('click', () => {
+  window.location.href = 'game.html';
+});
